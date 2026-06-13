ddc8b62ca16098758953cc33cb74ce3756b90b43
[Ernad Mujakic] <ernadmwork@gmail.com>
2026-06-13 14:40:01 -0500

    removed debug files

--- Debug/Src/003LED_button.d
+++
@@ -1,7 +0,0 @@
-Src/003LED_button.o: ../Src/003LED_button.c \
- /home/ernad/STM32CubeIDE/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h \
- /home/ernad/STM32CubeIDE/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h \
- /home/ernad/STM32CubeIDE/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
-/home/ernad/STM32CubeIDE/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
-/home/ernad/STM32CubeIDE/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h:
-/home/ernad/STM32CubeIDE/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
--- Debug/drivers/Src/stm32f407xx_gpio_driver.d
+++
@@ -1,8 +0,0 @@
-drivers/Src/stm32f407xx_gpio_driver.o: \
- ../drivers/Src/stm32f407xx_gpio_driver.c \
- /home/ernad/STM32CubeIDE/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h \
- /home/ernad/STM32CubeIDE/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h \
- /home/ernad/STM32CubeIDE/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h
-/home/ernad/STM32CubeIDE/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h:
-/home/ernad/STM32CubeIDE/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
-/home/ernad/STM32CubeIDE/MCU1/stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h:
